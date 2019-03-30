277f4b07481cec962c8bdf8e87d9b9052ae707e0
Josh <josh.arrowsmith@hotmail.com>
2019-03-30 22:30:06 +1000

    s

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
     position: absolute;
-    top: 10%;
+    top: 12%;
     width: 100%;
     overflow: hidden;
     @media screen and (max-width: 768px) {
