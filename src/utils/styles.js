6e055c6c867f729f7c053828bed6ce16582000ba
Josh <josh.arrowsmith@hotmail.com>
2019-04-01 11:14:36 +1000

    Global Styles added

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import Image from 'gatsby-image'
-import styled from '@emotion/styled-base'
-import { Global, css } from "@emotion/core"
+import { createGlobalStyle } from 'styled-components'
 
-export const GlobalStyle = props => (
-  <Global
-    {...props}
-    styles={css`
-      body {
-        margin: 0;
-      }
-      html {
-        font-family: sans-serif;
-        -ms-text-size-adjust: 100%;
-        -webkit-text-size-adjust: 100%;
-      }
-    `}
-  />
-)
+export const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
